094275198abe5c8c30ee12e8a135aba98ee24e15
manikajan123 <manika.murali@west.cmu.edu>
2017-03-13 22:50:44 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,16 @@ import {browserHistory } from 'react-router';
 import ImageNameViewer from './ImageNameViewer';
 import ImageUploader from './ImageUploader';
 
-
 class App extends Component {
    render() {
-    const Home = () => <h1>Welcome to the Image Uploader</h1>
-    return (
+   const Home = () => <h1>Welcome to the Image Uploader</h1>
+   return (
       <Router history={browserHistory}>
-        <Route path='/' component={Home} />
-        <Route path='/upload' component={ImageUploader} />
-        <Route path='/display' component={ImageNameViewer} />
+      <Route path='/' component={Home} />
+      <Route path='/upload' component={ImageUploader} />
+      <Route path='/display' component={ImageNameViewer} />
       </Router>
-    )
-  }
+   )
+   }
 }
-
-
 export default App
